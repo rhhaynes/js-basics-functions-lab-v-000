325ce28f9779367ebed9cc30da362683749b6502
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-20 20:07:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ function distanceTravelledInFeet(a, b){
 }
 
 function calculatesFarePrice(a, b){
-  let distance = distanceTravelledInFeet(a, b);
-  if distance 
+  let feet = distanceTravelledInFeet(a, b);
+  if (feet <= 400){
+    return 0;
+  } else if (feet > 400 && feet <= 2000) {
+    return (feet-400) * 0.02;
+  } else if (feet > 2000 && feet <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
 }
