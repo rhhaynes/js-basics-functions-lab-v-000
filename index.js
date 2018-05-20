81d39a9e1075f10c92db946f87e03770550cf492
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-20 19:57:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ function distanceFromHqInBlocks(pickupLocation){
   return Math.abs(pickupLocation - 42);
 }
 
+function distanceFromHqInFeet(){
+  return distanceFromHqInBlocks * 264;
+}
+
 function 
